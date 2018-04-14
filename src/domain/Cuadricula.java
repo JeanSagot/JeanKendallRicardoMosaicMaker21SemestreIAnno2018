512285da7e969cc469240e099966d13b37fccd88
JeanSagot <jeanpaulsagotvargas@gmail.com>
2018-04-14 11:47:35 -0600

    Primer commit: Inicio de interfaz y cargado de imagenes

---
+++ src/domain/Cuadricula.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+import java.awt.Graphics;
+import javax.swing.JComponent;
+
+/**
+ *
+ * @author jeanp
+ */
+public class Cuadricula {
+
+    
+    
+}
---
+++ src/proyecto2/Proyecto2.java
@@ -0,0 +1,17 @@
+package proyecto2;
+import Interface.MainFrame;
+/**
+ *
+ * @author jeanp
+ */
+public class Proyecto2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainFrame mf = new MainFrame();
+        mf.setVisible(true);
+    }
+    
+}
